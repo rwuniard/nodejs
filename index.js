1b56f11393011d74e2780d85c59fa29236f73b03
Ronson Wuniardy <rwuniard@hotmail.com>
2019-03-03 11:32:59 -0500

    adding nodemon and additional http intro

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-const Logger = require('./Logger');
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
 
-const logger = new Logger();
-logger.on('message', (data) => console.log('Called listener: ', data));
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.end('<h1>Homepage</h1>');
+    }
+});
 
-logger.log("Hello logger");
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
 
--- logger.js
+++ logger.js
@@ -9,4 +9,16 @@ class Logger extends EventEmitter {
         this.emit('message', {id: uuid.v4(), msg});
     }
 }
-module.exports = Logger;
+// module.exports = Logger;
+//==================================================
+// Below can be in another file if you uncomment the module.export and
+// const logger below.
+
+// const Logger = require('./Logger');
+
+const logger = new Logger();
+logger.on('message', (data) => console.log('Called listener: ', data));
+
+logger.log("Hello logger");
+logger.log("Hi");
+logger.log("hello");
